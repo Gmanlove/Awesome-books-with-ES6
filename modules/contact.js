c3db91ac8c5e2d8f0d2588dd5f07539c27c674ff
Gmanlove <emmagman3@gmail.com>
2023-02-20 16:52:52 +0100

    Update the index.js file

---
+++ modules/contact.js
@@ -0,0 +1,6 @@
+export const showContact = () => {
+  document.getElementById('id1').style.display = 'none';
+  document.getElementById('addBook').style.display = 'none';
+  document.getElementById('contact').style.display = 'block';
+};
+export default showContact;
